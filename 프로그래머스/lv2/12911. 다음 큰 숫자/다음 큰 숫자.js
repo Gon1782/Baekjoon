eaab607c271053783070db72ce5f4f3efba9a4ca
Gon <116235243+Gon1782@users.noreply.github.com>
2022-12-21 15:47:42 +0900

    Delete 프로그래머스/lv2 directory

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++
@@ -1,9 +0,0 @@
-function solution(n) {
-    let one = [...n.toString(2)].filter(x => x === "1").length;
-    let i = n+1;
-    while (1) {
-        if ([...i.toString(2)].filter(x => x === "1").length === one) break;
-        i++;
-    }
-    return i
-}
